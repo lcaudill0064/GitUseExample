059dfaa4676b01394b62eaf2c03bb665c41661c4
Logan Caudill <lmca276@uky.edu>
2020-08-20 13:06:56 -0700

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by user179507 on 8/20/20.
+//  Copyright © 2020 Logan Caudill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
